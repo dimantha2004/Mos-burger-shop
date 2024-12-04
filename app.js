2d49639de31f6a3685050cf18de69555bef5c0e0
dimantha2004 <Dimanthatheekshana12345@gmail.com>
2024-12-04 14:40:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const username = "nish"; 
+const password = "123"; 
+
+function login() {
+    let username = document.getElementById("USERNAME").value;
+    let password = document.getElementById("PASSWORD").value;
+
+    if (username === USERNAME && password === PASSWORD) {
+
+
+
+        
+        
+    } else {
+        alert("Password / Username Incorrect");
+    }
+}
+
